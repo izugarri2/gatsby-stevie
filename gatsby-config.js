cf44579bc1671ca63a3ac7a1a3ab842c8c68f4e5
Izugarri <57817632+Izugarri@users.noreply.github.com>
2021-09-10 17:18:58 +0200

    Update gatsby-config.js

--- gatsby-config.js
+++ gatsby-config.js
@@ -114,7 +114,7 @@ module.exports = {
       options: {
         name: `Gatsby Starter Blog`,
         short_name: `GatsbyJS`,
-        start_url: `/`,
+        start_url: `/static/`,
         background_color: `#ffffff`,
         theme_color: `#663399`,
         display: `minimal-ui`,
