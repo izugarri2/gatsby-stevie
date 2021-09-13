3270a36c568650642f48943f1ee186928cdcf106
Izugarri <57817632+Izugarri@users.noreply.github.com>
2021-09-13 19:57:37 +0200

    New arranque

---
+++ static/colaboraciones/js/javabox.js
@@ -0,0 +1,9 @@
+<!--
+var css = document.createElement('link');
+css.type = 'https://www.sitelevel.com/text/css';
+css.rel = 'https://www.sitelevel.com/stylesheet';
+css.href = 'https://www.sitelevel.com/css/searchbox.go?crid=sbklmpk2';
+document.getElementsByTagName("head")[0].appendChild(css);
+document.write("<!-- Begin siteLevel searchbox -->\n<form method=\"get\" action=\"https:\/\/www.sitelevel.com\/query\"  accept-charset=\"utf-8\">\n<div class=\"sl_search_box\">\n<input type=\"hidden\" name=\"crid\" value=\"sbklmpk2\">\n<input type=\"text\" name=\"query\" size=\"25\" maxlength=\"100\"> <button type=\"submit\">Search<\/button>\n\n\n<\/div>\n<\/form>\n<!-- End siteLevel searchbox -->");
+document.close();
+//-->
--- static/js/Youtube-videos.js
+++
@@ -1,9 +0,0 @@
-//<![CDATA[
-function insertar_YouTube(IDyoutube) {
-var output = "<a onclick=\"window.open('https://www.youtube.com/embed/" + IDyoutube + "?autoplay=1','ayuda','resizable=no,scrollbars=no,status=no,toolbar=no,directories=no,menubar=no,resizable=no,width=640,height=400,top=350,left=300')\"><img src=\"https://img.youtube.com/vi/" + IDyoutube + "/0.jpg\"  border=\"0\"><div><img src=\"/IMAGENES/Boton-play.gif\" border=\"0\"></a></div>";
-document.write (output);
-}
-//]]>
-<!--
-  function irA(menu){window.location.href = menu.options[menu.selectedIndex].value;}
-  // fin de javascript -->
