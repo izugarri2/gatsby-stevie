e3d2a694b20fc613087605f20c00078377b4e34b
Izugarri <57817632+Izugarri@users.noreply.github.com>
2022-08-14 21:46:23 +0200

    Update index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,7 @@
 import React from "react"
 
 export default function Home() {
-  return <div>Hello world!</div>
+  return <div>Search Stevie Gatsby</div>
+  <a href="https://www.steviewonder.es">Web stevie Wonder</a>
+
 }
