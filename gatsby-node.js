a9a840f4b2ff8120dc4087c54020acddc85f851e
Izugarri <57817632+Izugarri@users.noreply.github.com>
2022-08-14 21:03:49 +0200

    ini

--- gatsby-node.js
+++ gatsby-node.js
@@ -4,8 +4,6 @@ const { createFilePath } = require(`gatsby-source-filesystem`)
 exports.createPages = async ({ graphql, actions, reporter }) => {
   const { createPage } = actions
 
-  // Define a template for blog post
-  const blogPost = path.resolve(`./src/templates/blog-post.js`)
 
   // Get all markdown blog posts sorted by date
   const result = await graphql(
