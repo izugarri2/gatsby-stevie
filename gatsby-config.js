0d3ae611d3548fe6674270fd312a3832ed8ed576
izugarri2 <57878215+izugarri2@users.noreply.github.com>
2022-09-16 10:54:30 +0200

    Update gatsby-config.js

--- gatsby-config.js
+++ gatsby-config.js
@@ -3,23 +3,4 @@
  *
  * See: https://www.gatsbyjs.com/docs/gatsby-config/
  */
-module.exports = {
-    plugins: [
-    {
-      resolve: `gatsby-source-filesystem`,
-      options: {
-        path: `${__dirname}/static`,
-        name: `static`,
-      },
-    },
-    {
-      resolve: `gatsby-source-filesystem`,
-      options: {
-        path: `${__dirname}/`,
-        name: ``,
-      },
-    },
-    ...
-  ],
-}
 
