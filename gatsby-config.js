0a1040f51fdd2ab5c3cd6001d3690ada61953b95
izugarri2 <57878215+izugarri2@users.noreply.github.com>
2022-09-16 10:17:40 +0200

    Update gatsby-config.js

--- gatsby-config.js
+++ gatsby-config.js
@@ -3,4 +3,18 @@
  *
  * See: https://www.gatsbyjs.com/docs/gatsby-config/
  */
+module.exports = {
+  siteMetadata: {
+    title: `Your Site Name`,
+  },
+  plugins: [
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `static`,
+        path: `${__dirname}/`,
+      },
+    },
+  ],
+}
 
