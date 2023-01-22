3676f1ea5098d026f40ac7de16ca2c4757d5a738
Izugarri <57817632+Izugarri@users.noreply.github.com>
2023-01-22 12:22:12 +0100

    con plu

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,17 +1,64 @@
-/**
- * @type {import('gatsby').GatsbyConfig}
- */
 module.exports = {
   siteMetadata: {
-    title: `prueba`,
-    siteUrl: `https://www.yourdomain.tld`
+    siteUrl: "https://bobmcc.dev",
+    title: "bobmcc",
   },
-  plugins: ["gatsby-plugin-mdx", {
-    resolve: 'gatsby-source-filesystem',
+  plugins: [
+    "gatsby-plugin-styled-components",
+    "gatsby-plugin-image",
+    "gatsby-plugin-react-helmet",
+    "gatsby-plugin-react-svg",
+    "gatsby-plugin-sitemap",
+    {
     options: {
-      "name": "pages",
-      "path": "./src/pages/"
+      headers: {"/*": [ 
+         'Referrer-Policy: strict-origin-when-cross-origin'
+         'mergeSecurityHeaders: false'
+       ]},
+      allPageHeaders: {[
+      "Strict-Transport-Security: max-age=31536022; includeSubDomains; preload",
+    ]},
+      resolve: "gatsby-plugin-manifest",
+      options: {
+        icon: "src/images/favicon.svg",
+      },
     },
-    __key: "pages"
-  }]
+    {
+      resolve: "gatsby-plugin-mdx",
+      options: {
+        gatsbyRemarkPlugins: [
+          {
+            resolve: `gatsby-remark-autolink-headers`,
+            options: {
+              elements: ["h1"],
+            },
+          },
+        ],
+      },
+    },
+    "gatsby-plugin-sharp",
+    "gatsby-transformer-sharp",
+    {
+      resolve: "gatsby-source-filesystem",
+      options: {
+        name: "images",
+        path: "./src/images/",
+      },
+      __key: "images",
+    },
+    {
+      resolve: "gatsby-source-filesystem",
+      options: {
+        name: "pages",
+        path: "./src/pages/",
+      },
+      __key: "pages",
+    },
+    {
+      resolve: `gatsby-plugin-theme-ui`,
+      options: {
+        preset: "@theme-ui/preset-deep",
+      },
+    },
+  ],
 };
