f8247e1d6363ab035e37e82e6db7b7c1de8d258f
Izugarri <57817632+Izugarri@users.noreply.github.com>
2023-01-22 11:22:23 +0100

    arran

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,56 +1,17 @@
+/**
+ * @type {import('gatsby').GatsbyConfig}
+ */
 module.exports = {
   siteMetadata: {
-    siteUrl: "https://bobmcc.dev",
-    title: "bobmcc",
+    title: `prueba`,
+    siteUrl: `https://www.yourdomain.tld`
   },
-  plugins: [
-    "gatsby-plugin-styled-components",
-    "gatsby-plugin-image",
-    "gatsby-plugin-react-helmet",
-    "gatsby-plugin-react-svg",
-    "gatsby-plugin-sitemap",
-    {
-      resolve: "gatsby-plugin-manifest",
-      options: {
-        icon: "src/images/favicon.svg",
-      },
+  plugins: ["gatsby-plugin-mdx", {
+    resolve: 'gatsby-source-filesystem',
+    options: {
+      "name": "pages",
+      "path": "./src/pages/"
     },
-    {
-      resolve: "gatsby-plugin-mdx",
-      options: {
-        gatsbyRemarkPlugins: [
-          {
-            resolve: `gatsby-remark-autolink-headers`,
-            options: {
-              elements: ["h1"],
-            },
-          },
-        ],
-      },
-    },
-    "gatsby-plugin-sharp",
-    "gatsby-transformer-sharp",
-    {
-      resolve: "gatsby-source-filesystem",
-      options: {
-        name: "images",
-        path: "./src/images/",
-      },
-      __key: "images",
-    },
-    {
-      resolve: "gatsby-source-filesystem",
-      options: {
-        name: "pages",
-        path: "./src/pages/",
-      },
-      __key: "pages",
-    },
-    {
-      resolve: `gatsby-plugin-theme-ui`,
-      options: {
-        preset: "@theme-ui/preset-deep",
-      },
-    },
-  ],
+    __key: "pages"
+  }]
 };
