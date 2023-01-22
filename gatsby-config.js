c9cc27438a5626afa93e165c8f60041df61ee245
Izugarri <57817632+Izugarri@users.noreply.github.com>
2023-01-22 14:43:46 +0100

    pack

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,9 +1,7 @@
 /**
- * @type {import('gatsby').GatsbyConfig}
+ * Configure your Gatsby site with this file.
+ *
+ * See: https://www.gatsbyjs.com/docs/gatsby-config/
  */
-module.exports = {
-  siteMetadata: {
-    siteUrl: `https://www.yourdomain.tld`,
-  },
-  plugins: [],
-}
+// gatsby-config.js
+
